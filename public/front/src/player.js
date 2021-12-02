0d00f748d946e1b188fa1e79e9ea538d5f87d0cf
farid ghaderi <farid.51gh@gmail.com>
2021-12-02 17:12:18 +0330

    structuring project folders for backend

---
+++ public/front/src/player.js
@@ -0,0 +1,102 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const audio = new Audio();
+const musicPlayerContainer = document.querySelector(".music-player-container");
+const progressbar = document.querySelector(".progressbar");
+const controlboxPlayBtn = document.querySelector("#control-play-btn");
+function parseAudioTime(number) {
+    return (number / 60).toFixed(2);
+}
+function getSongsSrc(parent) {
+    let srcs = [];
+    parent.childNodes.forEach((child) => {
+        if (child instanceof Element) {
+            if (child.classList.contains("player-queue-song") &&
+                child.hasAttribute("src")) {
+                srcs.push(child.getAttribute("src"));
+            }
+        }
+    });
+    return srcs.reverse();
+}
+function updateProgressbar(duration) {
+    if (progressbar instanceof HTMLInputElement) {
+        progressbar.value = "0";
+        progressbar.setAttribute("max", parseAudioTime(duration));
+    }
+}
+function playSong(src) {
+    audio.src = `../../public/music/${src}`;
+    audio.play();
+    //Update progressbar's information
+    audio.onloadedmetadata = () => {
+        updateProgressbar(audio.duration);
+    };
+}
+function changePLayIcons(selectedElement) {
+    selectedElement.classList.replace("bi-play-circle-fill", "bi-pause-circle-fill");
+    if (controlboxPlayBtn) {
+        controlboxPlayBtn.classList.replace("bi-play-circle-fill", "bi-pause-circle-fill");
+    }
+}
+let queueSongs = [];
+if (controlboxPlayBtn) {
+    controlboxPlayBtn.addEventListener("click", () => {
+        if (audio.src) {
+            if (controlboxPlayBtn.classList.contains("bi-play-circle-fill")) {
+                audio.play();
+                controlboxPlayBtn.classList.replace("bi-play-circle-fill", "bi-pause-circle-fill");
+            }
+            else {
+                audio.pause();
+                controlboxPlayBtn.classList.replace("bi-pause-circle-fill", "bi-play-circle-fill");
+            }
+        }
+    });
+}
+if (musicPlayerContainer) {
+    const playerQueue = document.querySelector(".player-queue");
+    if (playerQueue) {
+        // #region Get all songs src from the queue
+        queueSongs = getSongsSrc(playerQueue);
+        //#endregion
+        // #region
+        //play and pause songs when the user
+        //clicks on them in the queue list
+        playerQueue.addEventListener("click", (e) => __awaiter(void 0, void 0, void 0, function* () {
+            if (e.target instanceof Element) {
+                if (e.target.id === "play-song-btn") {
+                    //play the selected song and change the play icons
+                    const selectedSongElement = e.target.parentElement.parentElement.parentElement;
+                    if (selectedSongElement) {
+                        playSong(selectedSongElement.dataset.src);
+                        changePLayIcons(e.target);
+                    }
+                }
+            }
+        }));
+    }
+}
+audio.addEventListener("timeupdate", () => {
+    if (progressbar instanceof HTMLInputElement) {
+        console.log("hey");
+        let value = parseAudioTime(audio.currentTime);
+        console.log(value + "/" + parseAudioTime(audio.duration));
+        progressbar.value = value;
+    }
+});
+if (progressbar && progressbar instanceof HTMLInputElement) {
+    progressbar.addEventListener("input", () => {
+        if (audio.src && progressbar.value) {
+            audio.currentTime = parseFloat(progressbar.value) * 60;
+        }
+    });
+}
---
+++ public/front/src/ui.js
@@ -0,0 +1,38 @@
+"use strict";
+// Auth page animations
+const loginForm = document.querySelector(".login-container");
+const signupForm = document.querySelector(".signup-container");
+const nav = document.querySelector("nav");
+if (loginForm && signupForm) {
+    const showSignupFormBtn = document.querySelector("#show-signup");
+    const showLoginFormBtn = document.querySelector("#show-login");
+    showSignupFormBtn.addEventListener("click", () => {
+        loginForm.classList.add("fade-out");
+        signupForm.classList.add("fade-in");
+        signupForm.classList.remove("hide");
+        loginForm.classList.add("hide");
+    });
+    showLoginFormBtn.addEventListener("click", () => {
+        signupForm.classList.add("fade-out");
+        loginForm.classList.add("fade-in");
+        loginForm.classList.remove("hide");
+        signupForm.classList.add("hide");
+    });
+}
+// #region Show and hide music player
+const showHidePlayer = document.querySelector(".display-player-btn");
+if (showHidePlayer) {
+    showHidePlayer.addEventListener("click", () => {
+        const musicPlayerContainer = document.querySelector(".music-player-container");
+        const playerWindow = document.querySelector(".music-player-container .player");
+        if (showHidePlayer.classList.contains("show")) {
+            showHidePlayer.classList.replace("show", "collapse");
+            playerWindow.style.transform = "translateY(120%)";
+        }
+        else {
+            showHidePlayer.classList.replace("collapse", "show");
+            playerWindow.style.transform = "translateY(0%)";
+        }
+    });
+}
+//#endregion
